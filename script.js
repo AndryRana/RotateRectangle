e65c588aebcb9e265567aa461fee8a61e1d265df
AndryRana <lsourcus@gmail.com>
2023-05-23 14:18:18 +0200

    Add addRectangle function to create a new rectangle , Add drawRectangles function to ensure that all the rectangles in the rectangles array are drawn on the canvas, add an mousedown event to passe the e.offsetX and e.offsetY as the position of the new rectangle,and the randomly generated width and height as the dimensions

--- script.js
+++ script.js
@@ -23,3 +23,41 @@ function drawRectangle(rect) {
   ctx.fillRect(-rect.width / 2, -rect.height / 2, rect.width, rect.height); // Dessinez le rectangle centré sur (0,0)
   ctx.restore(); // Rétablir l'état de transformation précédent
 }
+
+// Dessiner tous les rectangles stockés dans le tableau rectangles sur le canevas.
+function drawRectangles() {
+  /* 
+  Efface tous les dessins précédents sur le canevas et 
+  spécifie la zone rectangulaire à effacer, en commençant par le coin supérieur gauche (0, 0) et en s'étendant jusqu'à la largeur et la hauteur du canevas.
+  */
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < rectangles.length; i++) {
+    drawRectangle(rectangles[i]);// Invoque la fonction drawRectangle pour dessiner le rectangle individuel sur le canevas.
+  }
+}
+
+// Ajoute un nouveau rectangle au tableau des rectangles, puis appelle la fonction drawRectangles pour redessiner tous les rectangles sur le canevas.
+function addRectangle(x, y, width, height) {
+  const rect = {
+    x: x,
+    y: y,
+    width: width,
+    height: height,
+    color: getRandomColor(),
+    angle: 0,
+  };
+  rectangles.push(rect);
+  drawRectangles();
+}
+
+/*
+Au clic, la fonction addRectangle est appelée et les valeurs e.offsetX et e.offsetY sont utilisées pour définir la position du nouveau rectangle,
+et la largeur et la hauteur générées aléatoirement comme dimensions
+*/
+canvas.addEventListener("mousedown", (e) => {
+  const width = Math.floor(Math.random() * (100 - 20 + 1))
+  const height = Math.floor(Math.random() * (100 - 20 + 1)) + 20;
+  addRectangle(e.offsetX, e.offsetY, width, height);
+});
+
+
