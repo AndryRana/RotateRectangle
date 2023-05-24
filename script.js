970defca5c4ec2aee829897d439b77248df0938c
AndryRana <lsourcus@gmail.com>
2023-05-24 15:16:04 +0200

    Rotate and delete after 360- Repaint

--- script.js
+++ script.js
@@ -60,4 +60,108 @@ canvas.addEventListener("mousedown", (e) => {
   addRectangle(e.offsetX, e.offsetY, width, height);
 });
 
+/*
+Supprime un rectangle spécifié du tableau des rectangles, 
+puis appelle la fonction drawRectangles pour redessiner tous les rectangles sur le canevas.
+*/
+function removeRectangle(rect) {
+  const index = rectangles.indexOf(rect);
+  if (index > -1) {
+    rectangles.splice(index, 1);
+    drawRectangles();
+  }
+}
 
+let rotationsInProgress = 0; // Compteur de rotations en cours
+
+function rotateRectangle(rect) {
+  const rotationSpeed = 5;
+  let currentAngle = 0;
+  
+  // Cette fonction est appelée à plusieurs reprises à un intervalle fixe pour faire pivoter le rectangle.
+  function rotate() {
+    currentAngle += rotationSpeed; //Augmente la variable currentAngle de la valeur rotationSpeed, ce qui a pour effet de faire pivoter le rectangle de la valeur spécifiée.
+    rect.angle = currentAngle; //met à jour la propriété angle de l'objet rect avec la nouvelle valeur currentAngle. Cette propriété est utilisée pour suivre l'angle de rotation du rectangle.
+    drawRectangles(); //Redessiner tous les rectangles sur la toile et s'assurer que le rectangle pivoté est affiché correctement
+
+    if (currentAngle >= 360) {
+      clearInterval(rotationInterval);
+      rect.angle = 0; // Remettre l'angle à zéro
+      removeRectangle(rect); 
+      checkRepaint();
+    }
+  }
+
+  // Il s'agit d'une fonction interne nommée cancelRotation qui annule le processus de rotation lors d'un double-clic.
+  const cancelRotation = () => {
+    clearInterval(rotationInterval); //Efface l'intervalle défini par setInterval pour arrêter le processus de rotation. Le rotationInterval est la référence à l'intervalle renvoyé par setInterval.
+    rect.angle = 0; // Remettre l'angle à zéro
+    drawRectangles();
+    removeRectangle(rect);
+    checkRepaint();//repeindre d'autres rectangles sur le canevas.
+    canvas.removeEventListener('dblclick', cancelRotation);
+  };
+
+  // Définir l'intervalle de rotation
+  const rotationInterval = setInterval(rotate, 50);
+
+  canvas.addEventListener('dblclick', cancelRotation);
+}
+
+/*
+Vérifier la différence de surface entre les paires de rectangles 
+et la mise à jour de leurs couleurs sur la base de la plus petite différence
+*/
+function checkRepaint() {
+  let smallestDiff = Infinity; // Suivre la plus petite différence de surface entre des paires de rectangles
+  let pair = null; // Stocker la paire de rectangles dont la différence d'aire est la plus faible
+  /**
+   * une boucle imbriquée qui parcourt les rectangles du tableau rectangles pour comparer leurs surfaces et trouver la paire dont la différence est la plus faible. 
+    avec la plus petite différence.
+   */
+  for (let i = 0; i < rectangles.length - 1; i++) {
+    for (let j = i + 1; j < rectangles.length; j++) {
+      const area1 = rectangles[i].width * rectangles[i].height;
+      const area2 = rectangles[j].width * rectangles[j].height;
+      const diff = Math.abs(area1 - area2);
+      if (diff < smallestDiff) {
+        smallestDiff = diff;
+        pair = [rectangles[i], rectangles[j]];
+      }
+    }
+  }
+  /** Si une paire de rectangles présentant la plus petite différence a été trouvée,
+  Attribuer une nouvelle couleur aléatoire au premier rectangle de la paire (paire[0]) et à la deuxième paire[1]
+ */
+  if (pair) {
+    pair[0].color = getRandomColor();
+    pair[1].color = getRandomColor();
+    drawRectangles(); //Redessine tous les rectangles sur le canevas après avoir mis à jour leurs couleurs.
+  }
+}
+
+/**
+ * ajoute un récepteur d'événement dblclick au canevas pour rechercher un rectangle dans le tableau rectangles qui contient la position cliquée.
+ */
+canvas.addEventListener("dblclick", (e) => {
+  const rect = rectangles.find((r) => {
+    return (
+      e.offsetX >= r.x &&
+      e.offsetX <= r.x + r.width &&
+      e.offsetY >= r.y &&
+      e.offsetY <= r.y + r.height
+    );
+  });
+  if (rect) {
+    rotateRectangle(rect); //Appel de la fonction rotateRectangle, en passant le rectangle trouvé comme argument.
+  }
+});
+
+/**
+ * Lorsque le bouton est cliqué, 
+ * il déclenche la vérification de la plus petite différence entre les surfaces des rectangles 
+ * et met à jour leurs couleurs en conséquence.
+ */
+repaintBtn.addEventListener("click", () => {
+  checkRepaint();
+});
